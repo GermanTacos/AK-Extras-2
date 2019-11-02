b4ad6a6c2d333190424c4439308c41d3fd32abd1
GermanTacos <jakebuilds@gmail.com>
2019-11-02 18:41:12 -0400

    First version; PMAG 10/20/30, US PALM mag

---
+++ hooks/weaponfactorytweakdata.lua
@@ -0,0 +1,8 @@
+Hooks:PostHook( WeaponFactoryTweakData, "init", "AKExtras2Init", function(self)
+	
+--=========================--
+--       AK EXTRAS 2       --
+-- GERMANTACOS, KILLERWOLF --
+--=========================--
+	
+end )
