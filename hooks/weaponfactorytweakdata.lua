dfae60e7427f45a4b3d646a0b7aa3d44667205f3
IKillerwolfI <killerwolf.no1@gmail.com>
2020-02-28 19:47:11 +0100

    VG updated 1

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -28,7 +28,41 @@ function WeaponFactoryTweakData:akpack2_setup_overrides( part_type, part_table, 
 		if self.parts[part_id].type == part_type then
 			if self.parts[part_id].override and self.parts[part_id].override[base_part] then
 				for _, extra_part_id in pairs(part_table) do
-					self.parts[part_id].override[extra_part_id] = deep_clone(self.parts[part_id].override[base_part])					
+					if self.parts[extra_part_id] then
+						self.parts[part_id].override[extra_part_id] = deep_clone(self.parts[part_id].override[base_part])					
+					end
+				end
+			end
+		end
+	end
+end
+
+function WeaponFactoryTweakData:akpack2_setup_argument( argument, argument_value, method, part_type, part_table, weapon_or_table )
+	if method == "forbids_uses_parts" then
+		for id, part_id in pairs(self[weapon_or_table].uses_parts) do
+			if self.parts[part_id].type == part_type then
+				self.parts[part_id][argument] = self.parts[part_id][argument] or false
+				if self.parts[part_id][argument] == argument_value then
+					self.parts[part_id].forbids = self.parts[part_id].forbids or {}
+					for _, extra_part_id in pairs(part_table) do
+						if self.parts[extra_part_id] and not self.parts[part_id].forbids[extra_part_id] then
+							table.insert(self.parts[part_id].forbids, extra_part_id)
+						end
+					end
+				end
+			end
+		end
+	elseif method == "forbids_table" then
+		for id, part_id in pairs(weapon_or_table) do
+			if self.parts[part_id] and self.parts[part_id].type == part_type then
+				self.parts[part_id][argument] = self.parts[part_id][argument] or false
+				if self.parts[part_id][argument] == argument_value then
+					self.parts[part_id].forbids = self.parts[part_id].forbids or {}
+					for _, extra_part_id in pairs(part_table) do
+						if self.parts[extra_part_id] and not self.parts[part_id].forbids[extra_part_id] then
+							table.insert(self.parts[part_id].forbids, extra_part_id)
+						end
+					end
 				end
 			end
 		end
@@ -55,6 +89,9 @@ ak_parts_barrel_ext_762x39 = {
 	"wpn_fps_ass_ak_all_md_venom"
 }
 
+ak_parts_vg_univ = {
+}
+
 ak_parts_pg_bake = {
 	"wpn_fps_ass_ak_stamp_762_pg_akm",
 	"wpn_fps_ass_ak_stamp_545_pg_ak74",
@@ -62,6 +99,33 @@ ak_parts_pg_bake = {
 	"wpn_fps_ass_ak_krink_545_pg_aks74u",
 	"wpn_fps_ass_ak_krink_762_pg_akmsu",
 	"wpn_fps_ass_ak_stamp_gold_pg_akms"
+}
+
+ak_parts_lhg_all = {
+	"wpn_fps_ass_ak_stamp_762_lhg_akm",
+	"wpn_fps_ass_ak_stamp_366_lhg_vpo209",
+	"wpn_fps_ass_ak_stamp_gold_lhg_akms",
+	"wpn_fps_ass_ak_stamp_545_lhg_ak74",
+	"wpn_fps_ass_ak_stamp_762_lhg_ak103",
+	"wpn_fps_ass_ak_stamp_545_lhg_ak74m",
+	"wpn_fps_ass_ak_stamp_556_lhg_ak101",
+	"wpn_fps_ass_ak_all_lhg_moe",
+	"wpn_fps_ass_ak_all_lhg_zhukov",
+	"wpn_fps_ass_ak_all_lhg_caa",
+	"wpn_fps_ass_ak_all_lhg_ak200",
+	"wpn_fps_ass_ak_all_lhg_b10m",
+	"wpn_fps_ass_ak_all_lhg_b10",
+	"wpn_fps_ass_ak_all_lhg_hex",
+	"wpn_fps_ass_ak_all_lhg_tdi",
+	"wpn_fps_ass_ak_all_lhg_vent",
+	"wpn_fps_ass_ak_all_lhg_vs24",
+	"wpn_fps_ass_ak_all_lhg_b11",
+	"wpn_fps_ass_ak_all_lhg_xrsu",
+	"wpn_fps_ass_ak_all_lhg_goliaf",
+	"wpn_fps_ass_ak_all_lhg_ding",
+	"wpn_fps_ass_ak_all_lhg_alpha",
+	"wpn_fps_ass_ak_all_lhg_ivan",
+	"wpn_fps_ass_ak_all_lhg_guard"
 }
 
 ak_parts_lhg_akm = {
@@ -155,6 +219,16 @@ ak_variants_all_poly = {
 	"sand"
 }
 
+--- CAFCW ---
+if attach_tables then
+	for _, vg_id in pairs(attach_tables.Vertical_Grips) do
+		if self.parts[vg_id] then
+			table.insert(ak_parts_vg_univ, vg_id)
+		end
+	end
+end
+
+
 --=========================--
 --      COMPATIBILITY      --
 --=========================--
@@ -163,6 +237,8 @@ if BeardLib.Utils:FindMod("AKM") then
 	table.insert(ak_variants_pg_bake, "akm")
 	self:akpack2_setup_overrides( "barrel2", ak_parts_barrel_ext_762x39, "wpn_fps_upg_ass_ns_battle", "wpn_fps_ass_ak_stamp_762" )
 	self:akpack2_setup_overrides( "barrel2", ak_parts_barrel_ext_univ, "wpn_fps_upg_ass_ns_battle", "wpn_fps_ass_ak_stamp_762" )
+	self:akpack2_setup_argument( "allows_vg", false, "forbids_uses_parts", "handguard_lower", ak_parts_vg_univ, "wpn_fps_ass_ak_stamp_762" )
+	self:akpack2_setup_argument( "allows_vg", false, "forbids_table", "handguard_lower", ak_parts_vg_univ, ak_parts_lhg_all )
 end
 
 if BeardLib.Utils:FindMod("AK-74") then
@@ -192,5 +268,17 @@ self:akpack2_setup_variants( "st", ak_parts_st_akm, "akm", ak_variants_st_akm )
 self:akpack2_setup_variants( "lhg", ak_parts_lhg_ak74, "ak74", ak_variants_hg_ak74 )
 self:akpack2_setup_variants( "uhg", ak_parts_uhg_ak74, "ak74", ak_variants_hg_ak74 )
 self:akpack2_setup_variants( "st", ak_parts_st_ak74, "ak74", ak_variants_st_ak74 )
+
+table.insert(self.wpn_fps_pis_x_stech.uses_parts, "wpn_fps_pis_stech_s_standard")
+--- VG stuff
+for i, vg_id in pairs(ak_parts_vg_univ) do
+	if self.parts[vg_id] then
+		if self.wpn_fps_ass_ak_stamp_762 then
+			self.wpn_fps_ass_ak_stamp_762.override[vg_id] = self.wpn_fps_ass_ak_stamp_762.override[vg_id] or {}
+			self.wpn_fps_ass_ak_stamp_762.override[vg_id].override_weapon = {weapon_hold = "rpk", use_stance = "akm2_vg_ads"}
+		end
+	end
+end
+
 
 end)
