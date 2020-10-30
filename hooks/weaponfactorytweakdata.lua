15bdc7efda3d361373017d8c5e6afffb633a6945
IKillerwolfI <killerwolf.no1@gmail.com>
2020-10-30 01:40:47 +0100

    Handguard forbids
    - Added forbids for full handguards on partial handguards

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -69,6 +69,16 @@ function WeaponFactoryTweakData:akpack2_setup_argument( argument, argument_value
 	end
 end
 
+--- Forbid Setup --- wtf is that bs above
+function WeaponFactoryTweakData:akpack2_setup_forbid( mod_one, mod_two )
+	if self.parts[mod_one] and self.parts[mod_two] then
+		self.parts[mod_one].forbids = self.parts[mod_one].forbids or {}
+		table.insert(self.parts[mod_one].forbids, mod_two)
+		self.parts[mod_two].forbids = self.parts[mod_two].forbids or {}
+		table.insert(self.parts[mod_two].forbids, mod_one)
+	end
+end
+
 Hooks:PostHook( WeaponFactoryTweakData, "init", "AKExtras2Init", function(self)
 
 --=========================--
@@ -118,6 +128,32 @@ ak_parts_pg_bake = {
 	"wpn_fps_ass_ak_stamp_gold_pg_akms"
 }
 
+ak_parts_hg_all = {
+	"wpn_fps_ass_ak_all_hg_trax",
+	"wpn_fps_ass_ak_all_hg_cmrd",
+	"wpn_fps_ass_ak_all_hg_ufm",
+	"wpn_fps_ass_ak_all_hg_heat"
+}
+
+ak_parts_uhg_all = {
+	"wpn_fps_ass_ak_stamp_762_uhg_akm",
+	"wpn_fps_ass_ak_stamp_366_uhg_vpo209",
+	"wpn_fps_ass_ak_stamp_gold_uhg_akm",
+	"wpn_fps_ass_ak_stamp_545_uhg_ak74",
+	"wpn_fps_ass_ak_stamp_762_uhg_ak103",
+	"wpn_fps_ass_ak_stamp_545_uhg_ak74m",
+	"wpn_fps_ass_ak_stamp_556_uhg_ak101",
+	"wpn_fps_ass_ak_all_uhg_b19",
+	"wpn_fps_ass_ak_all_uhg_swiss",
+	"wpn_fps_ass_ak_all_uhg_goliaf"
+}
+
+ak_parts_uhg_all_attach = {
+	"wpn_fps_ass_ak_all_uhg_b19",
+	"wpn_fps_ass_ak_all_uhg_swiss",
+	"wpn_fps_ass_ak_all_uhg_goliaf"
+}
+
 ak_parts_lhg_all = {
 	"wpn_fps_ass_ak_stamp_762_lhg_akm",
 	"wpn_fps_ass_ak_stamp_366_lhg_vpo209",
@@ -126,6 +162,26 @@ ak_parts_lhg_all = {
 	"wpn_fps_ass_ak_stamp_762_lhg_ak103",
 	"wpn_fps_ass_ak_stamp_545_lhg_ak74m",
 	"wpn_fps_ass_ak_stamp_556_lhg_ak101",
+	"wpn_fps_ass_ak_all_lhg_moe",
+	"wpn_fps_ass_ak_all_lhg_zhukov",
+	"wpn_fps_ass_ak_all_lhg_caa",
+	"wpn_fps_ass_ak_all_lhg_ak200",
+	"wpn_fps_ass_ak_all_lhg_b10m",
+	"wpn_fps_ass_ak_all_lhg_b10",
+	"wpn_fps_ass_ak_all_lhg_hex",
+	"wpn_fps_ass_ak_all_lhg_tdi",
+	"wpn_fps_ass_ak_all_lhg_vent",
+	"wpn_fps_ass_ak_all_lhg_vs24",
+	"wpn_fps_ass_ak_all_lhg_b11",
+	"wpn_fps_ass_ak_all_lhg_xrsu",
+	"wpn_fps_ass_ak_all_lhg_goliaf",
+	"wpn_fps_ass_ak_all_lhg_ding",
+	"wpn_fps_ass_ak_all_lhg_alpha",
+	"wpn_fps_ass_ak_all_lhg_ivan",
+	"wpn_fps_ass_ak_all_lhg_guard"
+}
+
+ak_parts_lhg_all_attach = {
 	"wpn_fps_ass_ak_all_lhg_moe",
 	"wpn_fps_ass_ak_all_lhg_zhukov",
 	"wpn_fps_ass_ak_all_lhg_caa",
@@ -345,6 +401,16 @@ end
 --- Sight Stuff
 for i, o_id in pairs(ak_parts_sights_univ) do
 	self.parts.wpn_fps_ass_ak_all_ro_tt01.override[o_id] = {a_obj = "a_o_tt01"}
+end
+
+--- Handguard stuff
+for i, hg_id in pairs(ak_parts_hg_all) do
+	for i, uhg_id in pairs(ak_parts_uhg_all_attach) do
+		self:akpack2_setup_forbid( hg_id, uhg_id )
+	end 
+	for i, lhg_id in pairs(ak_parts_lhg_all_attach) do
+		self:akpack2_setup_forbid( hg_id, lhg_id )
+	end
 end
 
 end)
